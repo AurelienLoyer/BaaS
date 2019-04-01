1983b0b3fac7177573a9fb9eb7ef8a92d979c15b
Aurélien Loyer <aur.loy@gmail.com>
2019-04-01 15:10:06 -0400

    chore: edit postinstall for heroku

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
+  console.log(`🍺  App running on port ${process.env.PORT || 3000}`)
 }
 bootstrap();
