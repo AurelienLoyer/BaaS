e04d72b353b644841236105d9e5cd04dd838ef5c
Aurélien Loyer <aur.loy@gmail.com>
2019-05-24 22:13:32 -0400

    👀 test: add imports and providers on tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,11 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 describe('Users Controller', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+          secretOrPrivateKey: 'secretBeer',
+          signOptions: {
+            expiresIn: 3600,
+          },
+        }),
+      ],
+      providers: [
+        UsersService,
+      ],
       controllers: [UsersController],
     }).compile();
 
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+          secretOrPrivateKey: 'secretBeer',
+          signOptions: {
+            expiresIn: 3600,
+          },
+        }),
+      ],
+      providers: [
+        UsersService,
+        JwtStrategy
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
