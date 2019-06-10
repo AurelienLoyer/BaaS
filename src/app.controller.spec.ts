b06e2a373f832be90bc0f15b11169a59b7d17755
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-06-10 18:59:23 +0200

    test: add unit test for app controller

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+
+describe('Users Controller', () => {
+  let controller: AppController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  it('should return hello world', () => {
+    expect(controller.root()).toEqual({ message: 'Hello world ! 🦄' });
+  });
+
+  it('should call the sendFile method of the HTTP Response', () => {
+    const response = {
+      sendFile: jest.fn(),
+    };
+
+    controller.test('nestjs.png', response);
+    expect(response.sendFile).toHaveBeenCalledWith('img/nestjs.png', {
+      root: 'static',
+    });
+  });
+});
