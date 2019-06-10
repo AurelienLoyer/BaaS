460c6e5be7b23df36717eff309320084201bf110
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-06-10 19:13:13 +0200

    test: add unit test for users.controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, UseGuards, Post, Body, Req, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Post,
+  Body,
+  Req,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiUseTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
@@ -7,30 +16,29 @@ import { UserDto } from './user.dto';
 @Controller('api/v1/users')
 @ApiUseTags('users')
 export class UsersController {
+  constructor(public usersService: UsersService) {}
 
-    constructor(public usersService: UsersService) { }
+  @Get()
+  findAll(): any[] {
+    return this.usersService.getAll();
+  }
 
-    @Get()
-    findAll(): any[] {
-        return this.usersService.getAll();
+  @Post('login')
+  async login(@Body() userDto: UserDto): Promise<any> {
+    const userToken = await this.usersService.login(userDto);
+    if (userToken === undefined) {
+      throw new HttpException(`User not found`, HttpStatus.NOT_FOUND);
     }
+    return userToken;
+  }
 
-    @Post('login')
-    async login(@Body() userDto: UserDto): Promise<any> {
-        const userToken = await this.usersService.login(userDto);
-        if (userToken === undefined) {
-            throw new HttpException(`User not found`, HttpStatus.NOT_FOUND);
-        }
-        return userToken;
-    }
-
-    @Get('info')
-    @ApiBearerAuth()
-    @UseGuards(AuthGuard('jwt'))
-    getUserInfo(@Req() req) {
-        return {
-            ...req.user,
-            password: '******',
-        }
-    }
+  @Get('info')
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('jwt'))
+  getUserInfo(@Req() req) {
+    return {
+      ...req.user,
+      password: '******',
+    };
+  }
 }
