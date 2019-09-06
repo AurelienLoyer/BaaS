369ea14370f7516972314d311cc5e422fe72b7a3
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-09-06 13:08:46 +0200

    feat: step2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('🍺 Beer as a Service')
+    .setDescription('An API to retrieve beers informations')
+    .setVersion('1.0')
+    .addBearerAuth('Authorization', 'header')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+
   await app.listen(3000);
 }
 bootstrap();
